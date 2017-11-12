f49cde919e4e5fad5055b9dad07e60eb3f6c1057
Surya Rajasekaran <surya.rajasekaran.26@gmail.com>
2017-11-12 01:48:00 -0800

    fix setup

--- setup.sh
+++ setup.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 
 # setup influx
-sh C_influxdb/run.sh
+sh Container_influxdb/run.sh
 
 # setup database for metrics
 curl -X POST 'http://localhost:8086/query?pretty=true'  --data-urlencode "q=CREATE DATABASE metrics"
 # curl -G 'http://localhost:8086/query?pretty=true'  --data-urlencode "q=SHOW DATABASES"
 
 # setup grafana
-sh C_grafana/run.sh
+sh Container_grafana/run.sh
 
 # setup mongo
-sh C_mongodb/run.sh
+sh Container_mongodb/run.sh
 
 # setup webapp_one
-sh C_webapp_one/build.sh
-sh C_webapp_one/run.sh 1 8888
+sh Container_webapp_one/build.sh
+sh Container_webapp_one/run.sh 1 8888
