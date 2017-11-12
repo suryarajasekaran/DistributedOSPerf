85f247acba96682ae4078ada63995a7efde916dc
Surya Rajasekaran <surya.rajasekaran.26@gmail.com>
2017-11-12 02:23:43 -0800

    fix docker links

--- Controller/Scale.py
+++ Controller/Scale.py
@@ -1,12 +1,20 @@
 import subprocess
 import os
 
+webapp_port_mapper_dict = {
+                                "webapp_one"    : 8888,
+                                "webapp_two"    : 7777,
+                                "webapp_three"  : 6666,
+                                "webapp_four"   : 5555
+                          }
+
 def scaleup(webapp_name):
     print "This is a scaleup program"
     output = subprocess.check_output(["docker", "ps", "--format", "\"{{.Names}}\"", "-f", "name="+webapp_name])
     output_list = output.split("\n")
     scale_number = len(output_list)
-    subprocess.check_output(["sh", os.path.dirname(os.path.dirname(os.path.realpath(__file__)))+"/Container_"+webapp_name+"/run.sh", str(scale_number), str(8888 - scale_number + 1)])
+    subprocess.check_output(["sh", os.path.dirname(os.path.dirname(os.path.realpath(__file__))) +"/Container_" + webapp_name +"/run.sh", str(scale_number), str(webapp_port_mapper_dict[webapp_name] - scale_number + 1)])
+
 
 def scaledown(webapp_name):
     print "This is a scaledown program"
@@ -14,4 +22,4 @@ def scaledown(webapp_name):
     output_list = output.split("\n")
     scale_number = len(output_list)
     if scale_number <= 0:
-        subprocess.check_output(["sh", os.path.dirname(os.path.dirname(os.path.realpath(__file__))) + "/Container_"+webapp_name+"/delete.sh",str(scale_number-1)])
+        subprocess.check_output(["sh", os.path.dirname(os.path.dirname(os.path.realpath(__file__))) + "/Container_"+webapp_name+"/delete.sh", str(scale_number-1)])
