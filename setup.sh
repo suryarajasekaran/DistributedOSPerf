3ec56697fb2a5f3a012f85221950a1757059b8d6
Surya Rajasekaran <surya.rajasekaran.26@gmail.com>
2017-11-12 01:46:08 -0800

    fix names

--- setup.sh
+++ setup.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 
 # setup influx
-sh Container_influxdb/run.sh
+sh C_influxdb/run.sh
 
 # setup database for metrics
 curl -X POST 'http://localhost:8086/query?pretty=true'  --data-urlencode "q=CREATE DATABASE metrics"
 # curl -G 'http://localhost:8086/query?pretty=true'  --data-urlencode "q=SHOW DATABASES"
 
 # setup grafana
-sh Container_grafana/run.sh
+sh C_grafana/run.sh
 
 # setup mongo
-sh Container_mongodb/run.sh
+sh C_mongodb/run.sh
 
 # setup webapp_one
-sh Container_webapp_one/build.sh
-sh Container_webapp_one/run.sh 1 8888
+sh C_webapp_one/build.sh
+sh C_webapp_one/run.sh 1 8888
