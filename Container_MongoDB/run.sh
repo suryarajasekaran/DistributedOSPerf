ce1949addea3e057b4207b93b5c40a78368bf17b
Surya Rajasekaran <surya.rajasekaran.26@gmail.com>
2017-11-11 21:10:17 -0800

    adding mongo container

---
+++ Container_MongoDB/run.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+docker run --name mongodb -p 27017:27017 -d mongo
--- setup.sh
+++ setup.sh
@@ -6,6 +6,9 @@ sh Container_InfluxDB/run.sh
 # setup grafana
 sh Container_Grafana/run.sh
 
+# setup mongo
+sh Container_MongoDB/run.sh
+
 # setup webapp_one
 sh Container_WebApp_One/build.sh
 sh Container_WebApp_One/run.sh 1
